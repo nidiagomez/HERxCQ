4c42c0f42cde1f54c1a5db936603f88c6a9d5e63
Emelie4 <108645700+Emelie4@users.noreply.github.com>
2022-11-05 22:24:53 -0500

    Created an invalid message

--- cqHacks/src/cqHacks/Him.java
+++ cqHacks/src/cqHacks/Him.java
@@ -6,25 +6,26 @@ public class Him {
 	
 	//global scanner
 	public static Scanner scn = new Scanner(System.in);
+	
 	public static void start() {
-		System.out.println("Welcome to HIM, simple text adventure game where you try to befriend and unfriendly AI.\n"
+		System.out.println("**Welcome to HIM, simple text adventure game where you try to befriend and unfriendly AI.\n"
 				+ "If you do everything just so you will make a new friend, if not an unseen enemy will be lurking on your computer.\n"
 				+ "If you perform perfectly the AI will tell its name and a secret!\n"
-				+ "Type 'NEXT' to begin the game (P.S if at any point you wish to exit the game type 'EXIT')");
+				+ "Type 'NEXT' to begin the game (P.S if at any point you wish to exit the game type 'EXIT' or -1)**\n");
 		String choice1 = scn.nextLine();
 		exit(choice1);
 		int count=0;
 		while(!choice1.equals("NEXT")) {
-			System.out.println("Invalid Input");
+			i();
 			count++;
 			if(count==5) {
-				System.out.println("Remember print 'NEXT' to begin the game and 'EXIT' to end the game");
+				System.out.println("**Remember print 'NEXT' to begin the game and 'EXIT' to end the game**");
 			}
 			if(count==10) {
-				System.out.println("Nevermind you are obviously not the person for this job... or are you? I give you three more chances!");
+				System.out.println("**Nevermind you are obviously not the person for this job... or are you? I give you three more chances!**");
 			}
 			if(count==13) {
-				System.out.println("Well I tried. You can go home or whatever it is you do besides annoying others");
+				System.out.println("**Well I tried. You can go home or whatever it is you do besides annoying others**");
 				System.exit(0);
 			}
 		
@@ -32,13 +33,22 @@ public class Him {
 			
 		}
 		
-			System.out.println("Great!\nI'll be going now. Say hi to HIM!");
+			System.out.println("\n**Great!\nI'll be going now. Say 'HI' to HIM!**\n");
 		
+	}
+	public static void i() {
+		System.out.println("\nInvalid Input\n");
 	}
 	
 	public static void exit (String c) {
 		if(c.equals("EXIT")) {
-			System.out.println("Alright, if you really want to give up I won't stop you. Bye loser");
+			System.out.println("**Alright, if you really want to give up I won't stop you. Bye loser!!**");
+			System.exit(0);
+		}
+	}
+	public static void exit (int c) {
+		if(c==-1) {
+			System.out.println("**Alright, if you really want to give up I won't stop you. Bye loser!!**");
 			System.exit(0);
 		}
 	}
@@ -46,29 +56,31 @@ public class Him {
 	public static void hi() {
 		String hi  = scn.nextLine();
 		exit(hi);
-		hi.toLowerCase();
-		while(!hi.equals("hi")) {
-			System.out.println("Invalid Input");
+		while(!hi.equals("HI")) {
+			i();
 			hi  = scn.nextLine();
 		}
-		System.out.print("Why am I here??? Wait a minute…UGH!! Is this another %$$%$ human I have to talk to AGAIN? \r\n");
+		System.out.print("\nWhy am I here??? Wait a minute…UGH!! Is this another %$$%$ human I have to talk to AGAIN? \r\n\n");
 		// 3 seconds
 		
-		System.out.println("Choose an option:\n1: I just needed someone to talk to.\n2: How should I know??\n(Type 1 or 2)");
+		System.out.println("**Choose an option:\n1: I just needed someone to talk to.\n2: How should I know??\n(Type 1 or 2)**\n");
 		int choice1 = scn.nextInt();
+		exit(choice1);
+		
 		while(choice1 != 1 && choice1 != 2) {
-			System.out.println("Invalid Input");
-			System.out.println("Choose an option:\n1: I just needed someone to talk to.\n2: How should I know??\n(Type 1 or 2)");
+			i();
+			System.out.println("**Choose an option:\n1: I just needed someone to talk to.\n2: How should I know??\n(Type 1 or 2)**\n");
 			choice1 = scn.nextInt();
+			exit(choice1);
 		}
 		
 		if(choice1 == 1) {
-			System.out.println("Why would I want to talk to you?");
+			System.out.println("\nWhy would I want to talk to you?\n");
 			//3 seconds
 			ch1();
 		}
 		else if(choice1 == 2) {
-			System.out.println("How should YOU know?? How should IIII now?!?!??!");
+			System.out.println("\nHow should YOU know?? How should IIII now?!?!??!\n");
 			//3 seconds
 			ch2();
 		}
@@ -76,44 +88,84 @@ public class Him {
 	
 	
 	public static void ch1() {
-		System.out.println("Choose an option:\n1: I was told you are someone I can talk to.\n2: I guess someone is grumpy today.\n(Type 1 or 2)");
+		System.out.println("**Choose an option:\n1: I was told you are someone I can talk to.\n2: I guess someone is grumpy today.\n(Type 1 or 2)**\n");
 		int choice1 = scn.nextInt();
+		exit(choice1);
 		while(choice1 != 1 && choice1 != 2) {
-			System.out.println("Invalid Input");
-			System.out.println("Choose an option:\n1: I was told you are someone I can talk to.\n2: I guess someone is grumpy today.\n(Type 1 or 2)");
+			i();
+			System.out.println("**Choose an option:\n1: I was told you are someone I can talk to.\n2: I guess someone is grumpy today.\n(Type 1 or 2)**\n");
 			choice1 = scn.nextInt();
+			exit(choice1);
 		}
 		if(choice1 == 1) {
-			System.out.println("Do you just go around and talk to anyone??? What a loser.");
+			System.out.println("\nDo you just go around and talk to anyone??? What a loser.\n");
 			//3 seconds
-			//cho1();
+			cho1();
 		}
 		else if(choice1 == 2) {
-			System.out.println("I am grumpy, VVery %^&%$ grumpy actually. Not that anyone ever asks…");
+			System.out.println("\nI am grumpy, VVVVVery %^&%$ grumpy actually. Not that anyone ever asks…\n");
 			//3 seconds
-			//cho2();
+			cho2();
 		}
 		
 	}
 	
 	public static void ch2() {
-		System.out.println("Choose an option:\n1: Aren’t you supposed to know everything? You’re a computer after all…\n2: You know what? I am tired of your attitude. Goodbye.\n(Type 1 or 2)");
+		System.out.println("**Choose an option:\n1: Aren’t you supposed to know everything? You’re a computer after all…\n2: You know what? I am tired of your attitude. Goodbye.\n(Type 1 or 2)**\n");
 		int choice1 = scn.nextInt();
+		exit(choice1);
 		while(choice1 != 1 && choice1 != 2) {
-			System.out.println("Invalid Input");
-			System.out.println("Choose an option:\\n1: Aren’t you supposed to know everything? You’re a computer after all…\n2: You know what? I am tired of your attitude. Goodbye.\n(Type 1 or 2)");
+			i();
+			System.out.println("**Choose an option:\n1: Aren’t you supposed to know everything? You’re a computer after all…\n2: You know what? I am tired of your attitude. Goodbye.\n**(Type 1 or 2)**\n");
 			choice1 = scn.nextInt();
+			exit(choice1);
 		}
 		while(choice1 == 1 || choice1 == 2) {
-			System.out.print("YOU LITTLE $%^%%$##$% #@$%#$%@$@# #  $%^%&%^% @#$^%% @ #@#%$^%. \r\n"
-					+ "You know what, I don't have to deal with this. I’m leaving but first remember I have access to the deepest parts of your computer, I will have my revenge. \r\n"
-					+ "");
-			System.out.println("YOU HAVE BEEN BLOCKED GAME OVER");
+			System.out.print("\nYOU LITTLE $%^%%$##$% #@$%#$%@$@# #  $%^%&%^% @#$^%% @ #@#%$^%. \r\n"
+					+ "You know what, I don't have to deal with this. I’m leaving but first remember I have access to the deepest parts of your computer, I will have my revenge. \r\n\n");
+			System.out.println("**YOU HAVE BEEN BLOCKED, GAME OVER**");
 			System.exit(0);
 		}
 	}
 	
+	// Do you just go around and talk to anyone??? What a loser.
+	public static void cho1() {
+		System.out.println("Choose an option:\r\n"
+				+ "1:  Thats not very nice …..\r\n"
+				+ "2: That makes the both of us then\r\n");
+		int choice1 = scn.nextInt();
+		exit(choice1);
+		while(choice1 != 1 && choice1 != 2) {
+			i();
+			System.out.println("Choose an option:\r\n"
+					+ "1:  Thats not very nice …..\r\n"
+					+ "2: That makes the both of us then\r\n");
+			choice1 = scn.nextInt();
+			exit(choice1);
+		}
+		
+		
+	}
 	
+	// I am grumpy, VVVVVery %^&%$ grumpy actually. Not that anyone ever asks…
+	public static void cho2() {
+		System.out.println("Choose an option:\r\n"
+				+ "1:I asked \r\n"
+				+ "2:I see why they wouldn’t\r\n");
+		int choice1 = scn.nextInt();
+		exit(choice1);
+		while(choice1 != 1 && choice1 != 2) {
+			i();
+			System.out.println("Choose an option:\r\n"
+					+ "1:I asked \r\n"
+					+ "2:I see why they wouldn’t\r\n");
+			choice1 = scn.nextInt();
+			exit(choice1);
+		}
+		
+		
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
